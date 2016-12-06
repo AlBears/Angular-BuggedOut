a38bfc952a4152db8a5da020fb452c1ad336ea1e
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-12-06 11:40:45 +0300

    routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BugModule } from './bugs/bug.module';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent }   from './app.component';
 @NgModule({
   imports:      [ BrowserModule,
-                  BugModule ],
+                  BugModule,
+                  AppRoutingModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
--- app/bugs/bug.module.ts
+++ app/bugs/bug.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { BugRoutingModule } from './bug-routing.module';
 
 import { BugListComponent } from './bug-list/bug-list.component';
 
 @NgModule({
-    imports: [ SharedModule ],
+    imports: [ 
+        SharedModule,
+        BugRoutingModule
+         ],
     exports: [],
     declarations: [
         BugListComponent
