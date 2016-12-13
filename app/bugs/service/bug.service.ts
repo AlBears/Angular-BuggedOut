173a28b74804b41169b37f6fb4976fd25eed3454
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-12-13 11:09:19 +0300

    parsing updates

--- app/bugs/service/bug.service.ts
+++ app/bugs/service/bug.service.ts
@@ -26,7 +26,7 @@ export class BugService {
 
     changedListener(): Observable<any> {
         return Observable.create(obs => {
-            this.bugsDbRef.on('child_changed', bug => {
+            this.bugsDbRef.on("child_changed", bug => {
                 const updatedBug = bug.val() as Bug;
                 updatedBug.id = bug.key;
                 obs.next(updatedBug);
