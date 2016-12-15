42f6feb643735b142d6b27b51e9a8271e6092e3e
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-12-15 08:15:22 +0300

    custom pipes severity, status

--- app/shared/shared.module.ts
+++ app/shared/shared.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { StatusPipe } from './pipe/custom.pipe';
+import { SeverityPipe } from './pipe/severity.pipe';
+
 @NgModule({
     imports: [ CommonModule ],
-    exports: [ CommonModule ],
-    declarations: [ ],
+    exports: [ CommonModule, StatusPipe, SeverityPipe ],
+    declarations: [ StatusPipe, SeverityPipe ]
 })
 export class SharedModule { }
